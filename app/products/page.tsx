a307f5d09fe83a2c0fce6614e8692671d46de382
James Kurgat <jaykurgat@gmail.com>
2026-09-23 02:59:59 -0700

    hardening: defer database pages to runtime

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import Link from 'next/link'
 import { getAllProducts } from '@/lib/storefront/getAllProducts'
 import ProductCard from '@/components/ProductCard'
