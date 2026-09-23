fb25266c6249b8e6c88bb96d3736f482e6378e97
James Kurgat <jaykurgat@gmail.com>
2026-09-23 16:06:35 -0700

    Show exact selected supplier variant image

---
+++ components/ImportVariantGallery.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import ProductGallery from '@/components/ProductGallery'
+import { useImportVariantContext } from './ImportVariantContext'
+
+export default function ImportVariantGallery({
+  title,
+  fallbackImages,
+}: {
+  title: string
+  fallbackImages: string[]
+}) {
+  const { selectedVariant } = useImportVariantContext()
+
+  const images = selectedVariant?.imageUrl
+    ? [selectedVariant.imageUrl, ...fallbackImages.filter((image) => image !== selectedVariant.imageUrl)]
+    : fallbackImages
+
+  return <ProductGallery title={title} imageUrls={images} />
+}
