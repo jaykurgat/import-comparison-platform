9385bbbea9038606003b41bf3d8dfc10d735bc1b
James Kurgat <jaykurgat@gmail.com>
2026-09-26 10:28:34 -0700

    Fix category directory container markup

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -90,6 +90,7 @@ export default async function ProductsPage({
 {category.name}
                     </Link>
                   ))}
+                  </div>
                 </div>
               )}
 
